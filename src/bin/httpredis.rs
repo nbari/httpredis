5bfe0848d646c5dbdb66702c13ff052240eda526
Eli Yukelzon <reflog@gmail.com>
2021-06-11 11:24:21 +0300

    killed the unwraps, optimized string alloc (#1)

--- src/bin/httpredis.rs
+++ src/bin/httpredis.rs
@@ -1,8 +1,9 @@
 // use bytes::Bytes;
 use chrono::prelude::*;
 use httpredis::options;
+use httpredis::options::Redis;
 use std::net::{IpAddr, Ipv4Addr};
-use std::process;
+
 use std::str::FromStr;
 use std::time::Duration;
 use tokio::{
@@ -10,19 +11,14 @@ use tokio::{
     net::TcpStream,
     time::timeout,
 };
-use tokio_native_tls::TlsConnector;
+
+use httpredis::errors::Error::{IOError, TimeoutError};
 use warp::http::StatusCode;
 use warp::Filter;
 
 #[tokio::main]
-async fn main() {
-    let redis: options::Redis = match options::new() {
-        Ok(o) => o,
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1);
-        }
-    };
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let redis: options::Redis = options::new()?;
 
     let port = redis.port;
 
@@ -45,86 +41,87 @@ async fn main() {
 
     let args = warp::any().map(move || redis.clone());
     let state_route = warp::any().and(args).and_then(state_handler);
-    warp::serve(state_route).run((addr, port)).await
+    warp::serve(state_route).run((addr, port)).await;
+    Ok(())
 }
 
 // state_handler return HTTP 100 if role:master otherwise 200
 // OK, otherwise HTTP 503 Service Unavailable
 async fn state_handler(redis: options::Redis) -> Result<impl warp::Reply, warp::Rejection> {
-    let stream = match timeout(Duration::from_secs(3), TcpStream::connect(&redis.host)).await {
-        Ok(conn) => match conn {
-            Ok(conn) => match TlsConnector::from(redis.tls)
-                .connect(&redis.host, conn)
-                .await
-            {
-                Ok(s) => s,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    process::exit(1);
-                }
-            },
-            Err(e) => {
-                eprintln!("{}", e);
-                process::exit(1);
-            }
-        },
-        Err(e) => {
-            eprintln!("Timeout :{}", e);
-            process::exit(1);
-        }
-    };
+    let stream = timeout(Duration::from_secs(3), TcpStream::connect(&redis.host))
+        .await
+        .map_err(TimeoutError)?
+        .map_err(IOError)?;
 
     let mut buf = BufStream::new(stream);
 
+    let result = check_master_status(redis, &mut buf)
+        .await
+        .map_err(IOError)?;
+    Ok(result)
+}
+
+async fn check_master_status(
+    redis: Redis,
+    buf: &mut BufStream<TcpStream>,
+) -> Result<StatusCode, std::io::Error> {
     // AUTH
     if let Some(pass) = redis.pass {
         let msg = format!("AUTH {}", pass);
-        buf.write_all(msg.as_bytes()).await.unwrap();
-        buf.write_all(b"\r\n").await.unwrap();
-        buf.flush().await.unwrap();
+        buf.write_all(msg.as_bytes()).await?;
+        buf.write_all(b"\r\n").await?;
+        buf.flush().await?;
 
         let mut buffer = String::new();
-        buf.read_line(&mut buffer).await.unwrap();
+        buf.read_line(&mut buffer).await?;
         if "+OK\r\n" != buffer {
             return Ok(StatusCode::UNAUTHORIZED);
         }
     }
 
     // role:master
-    buf.write_all(b"info replication").await.unwrap();
-    buf.write_all(b"\r\n").await.unwrap();
-    buf.flush().await.unwrap();
+    buf.write_all(b"info replication").await?;
+    buf.write_all(b"\r\n").await?;
+    buf.flush().await?;
 
     let mut is_master = false;
+    let mut line = String::new();
     loop {
-        let mut line = String::new();
-        buf.read_line(&mut line).await.unwrap();
+        line.clear();
+        buf.read_line(&mut line).await?;
         if line.starts_with("role:master") {
             is_master = true;
         } else if line == "\r\n" {
             break;
         }
     }
-    if is_master {
+    return if is_master {
         // uptime
         // to prevent having multiple masters
         // an old-master when starting can start has master before going into replicaof
-        buf.write_all(b"info server").await.unwrap();
-        buf.write_all(b"\r\n").await.unwrap();
-        buf.flush().await.unwrap();
+        buf.write_all(b"info server").await?;
+        buf.write_all(b"\r\n").await?;
+        buf.flush().await?;
+        let mut line = String::new();
         loop {
-            let mut line = String::new();
-            buf.read_line(&mut line).await.unwrap();
+            line.clear();
+            buf.read_line(&mut line).await?;
             if line.starts_with("uptime_in_seconds:") {
                 let v: Vec<&str> = line.split_terminator(':').collect();
-                if v[1].trim().parse::<usize>().unwrap() > 10 {
-                    return Ok(StatusCode::OK);
+                if v[1]
+                    .trim()
+                    .parse::<usize>()
+                    .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "Cannot parse!"))?
+                    > 10
+                {
+                    break;
                 }
             } else if line == "\r\n" {
-                return Ok(StatusCode::OK);
+                break;
             }
         }
+        Ok(StatusCode::OK)
     } else {
-        return Ok(StatusCode::SERVICE_UNAVAILABLE);
-    }
+        Ok(StatusCode::SERVICE_UNAVAILABLE)
+    };
 }
